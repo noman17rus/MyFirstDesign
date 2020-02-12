1de97d1b6064bc288a22d0f44918f7b2f8343a76
noman17rus <noman171991@gmail.com>
2020-02-12 21:05:03 +0400

    create main activity

--- app/src/main/java/com/example/myfirstdesign/MainActivity.java
+++ app/src/main/java/com/example/myfirstdesign/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.myfirstdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Spinner sp;
+    ScrollView sv_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+        tv.setText("Выберите объект анализа");
+        sp = (Spinner) findViewById(R.id.sp_1);
+
     }
 }
